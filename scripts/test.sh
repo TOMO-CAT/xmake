9685c792bb51f2a6a3dd915b846f77f86888a157
TOMOCAT <TOMO-CAT@github.com>
2024-10-25 23:36:35 +0800

    [Hotfix]: test script install libtool fail

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,13 @@ set -e
 export CPP="clang++ -E"
 export CC="clang"
 export CXX="clang++"
-export LD="clang++"
+
+# export LD="clang++"
+# 
+# install libtool fail
+# https://github.com/conan-io/conan-center-index/issues/25251
+unset LD
+
 export XMAKE_ROOT="y"
 
 # unset CPLUS_INCLUDE_PATH && C_INCLUDE_PATH envs for cross build situation
--- tests/projects/package/components/test.lua
+++ tests/projects/package/components/test.lua
@@ -1,4 +1,27 @@
 function main(t)
+    -- install libtool failed
+    --
+    -- https://github.com/conan-io/conan-center-index/issues/25251
+    --
+    -- => installing libtool .. (1/make) error: @programdir/core/sandbox/modules/os.lua:273: /usr/bin/make  install-recursive
+    -- make[1]: Entering directory '/home/cat/.xmake/cache/packages/2410/l/libtool/2.4.7/source'
+    -- Making install in .
+    -- make[2]: Entering directory '/home/cat/.xmake/cache/packages/2410/l/libtool/2.4.7/source'
+    -- make[3]: Entering directory '/home/cat/.xmake/cache/packages/2410/l/libtool/2.4.7/source'
+    --  /usr/bin/mkdir -p '/home/cat/.xmake/packages/l/libtool/2.4.7/f710939d070045c099f49e266ac2733e/bin'
+    --  /usr/bin/install -c libtool '/home/cat/.xmake/packages/l/libtool/2.4.7/f710939d070045c099f49e266ac2733e/bin'
+    --  /usr/bin/mkdir -p '/home/cat/.xmake/packages/l/libtool/2.4.7/f710939d070045c099f49e266ac2733e/lib'
+    --  /bin/bash ./libtool   --mode=install /usr/bin/install -c   libltdl/libltdl.la '/home/cat/.xmake/packages/l/libtool/2.4.7/f710939d070045c099f49e266ac2733e/lib'
+    -- libtool: install: /usr/bin/install -c libltdl/.libs/libltdl.so.7.3.2 /home/cat/.xmake/packages/l/libtool/2.4.7/f710939d070045c099f49e266ac2733e/lib/libltdl.so.7.3.2
+    -- /usr/bin/install: cannot stat 'libltdl/.libs/libltdl.so.7.3.2': No such file or directory
+    -- make[3]: *** [Makefile:1081: install-libLTLIBRARIES] Error 1
+    -- make[3]: Leaving directory '/home/cat/.xmake/cache/packages/2410/l/libtool/2.4.7/source'
+    -- make[2]: *** [Makefile:1966: install-am] Error 2
+    -- make[2]: Leaving directory '/home/cat/.xmake/cache/packages/2410/l/libtool/2.4.7/source'
+    -- make[1]: *** [Makefile:1648: install-recursive] Error 1
+    -- make[1]: Leaving directory '/home/cat/.xmake/cache/packages/2410/l/libtool/2.4.7/source'
+    -- make: *** [Makefile:1959: install] Error 2
+
 
     -- freebsd ci is slower
     if is_host("bsd") then
