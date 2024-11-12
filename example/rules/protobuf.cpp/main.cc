6215759ed4ecd3b0096b9ac0c6b28b3150fb1169
TOMO-CAT <TOMO-CAT@github.com>
2024-11-12 19:35:48 +0800

    [Offline]: protobuf example

---
+++ example/rules/protobuf.cpp/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// 支持两种路径 include 头文件
+#include "test.pb.h"
+#include "proto/test.pb.h"
+
+int main() {
+    std::cout << "test protobuf.cpp rule" << std::endl;
+    return 0;
+}
