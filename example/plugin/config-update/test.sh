76d147426855e2be8f643b2c0016164c1bf4fe61
TOMO-CAT <TOMO-CAT@github.com>
2024-11-15 21:07:27 +0800

    [Feature]: xmake config-update plugin

---
+++ example/plugin/config-update/test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function info() {
+  (>&2 printf "[\e[34m\e[1mINFO\e[0m] $*\n")
+}
+
+function error() {
+  (>&2 printf "[\033[0;31mERROR\e[0m] $*\n")
+}
+
+function warning() {
+  (>&2 printf "[\033[0;33mWARNING\e[0m] $*\n")
+}
+
+function ok() {
+  (>&2 printf "[\e[32m\e[1m OK \e[0m] $*\n")
+}
+
+contains_string() {
+    local file="$1"
+    local string="$2"
+    
+    # `--` 用于告诉 grep 选项结束, 后面的是要搜索的字符串
+    if grep -q -- "$string" "$file"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+assert_contains_string() {
+    local file="$1"
+    local string="$2"
+
+    if contains_string "$file" "$string"; then
+        ok "File $file contains string $string"
+    else
+        error "File $file does not contain string $string"
+        exit 1
+    fi
+}
+
+xmake f -cv
+
+xmake f --policies=package.precompiled:n,package.install_only
+xmake f --export=build/config.txt
+assert_contains_string "build/config.txt" "package.precompiled:n"
+
+# xmake f --cxxflags="-DTEST -O3 -Wall"
+xmake cu --cxxflags="-DTEST -O3 -Wall"
+xmake f --export=build/config.txt
+assert_contains_string "build/config.txt" "package.precompiled:n"
+assert_contains_string "build/config.txt" "-DTEST -O3 -Wall"
+
+# xmake f -m coverage
+xmake cu -m coverage
+xmake f --export=build/config.txt
+assert_contains_string "build/config.txt" "package.precompiled:n"
+assert_contains_string "build/config.txt" "-DTEST -O3 -Wall"
+assert_contains_string "build/config.txt" "coverage"
---
+++ xmake/plugins/config-update/main.lua
@@ -0,0 +1,48 @@
+-- copy from https://github.com/SirLynix/xmake-plugins/blob/main/config-update/main.lua
+import("core.base.option")
+import("core.base.task")
+import("core.cache.localcache")
+import("core.project.config")
+import("core.project.project")
+
+-- filter option
+function _option_filter(name)
+    local options =
+    {
+        target      = true
+    ,   file        = true
+    ,   root        = true
+    ,   yes         = true
+    ,   quiet       = true
+    ,   confirm     = true
+    ,   project     = true
+    ,   verbose     = true
+    ,   diagnosis   = true
+    ,   require     = true
+    ,   export      = true
+    ,   import      = true
+    }
+    return not options[name]
+end
+
+function main()
+    -- lock the whole project
+    project.lock()
+
+    local options = localcache.get("config", "options") or {}
+
+    for name, value in pairs(option.options()) do
+        if _option_filter(name) then
+            options[name] = value
+        end
+    end
+
+    print("New options values", options)
+
+    options["check"] = true
+
+    task.run("config", options)
+
+    -- unlock the whole project
+    project.unlock()
+end
