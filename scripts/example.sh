2faf2fd50717966e56dbaae4b771c88b43d773f6
zifeng.yang <zifeng.yang@zelostech.com>
2024-12-03 22:35:05 +0800

    [Offline]: example workflows

--- scripts/example.sh
+++ scripts/example.sh
@@ -8,6 +8,14 @@ export XMAKE_PROGRAM_DIR="${PWD}/xmake"
 
 mapfile -t test_scripts < <(find example -type f -name "test.sh")
 
+if [ -z "${GITHUB_ACTIONS+x}" ]; then
+    info "not running in github actions"
+    SHOW_OUTPUT=false
+else
+    info "running in github actions, we will show all output"
+    SHOW_OUTPUT=true
+fi
+
 for test_script in "${test_scripts[@]}"; do
     script_dir=$(dirname "$test_script")
 
@@ -15,14 +23,23 @@ for test_script in "${test_scripts[@]}"; do
 
     cd "$script_dir" || continue
 
-    output=$(bash test.sh 2>&1)
-
-    if [[ $? -ne 0 ]]; then
-        error "Script failed in example [$script_dir] with output:"
+    if [ "${SHOW_OUTPUT}" == true ]; then
         echo "-----------------------------------------------------------------"
-        echo "$output"
+        bash test.sh
+        if [[ $? -ne 0 ]]; then
+            error "Script failed in example [$script_dir]"
+            exit 1
+        fi
         echo "-----------------------------------------------------------------"
-        exit 1
+    else
+        output=$(bash test.sh 2>&1)
+        if [[ $? -ne 0 ]]; then
+            error "Script failed in example [$script_dir] with output:"
+            echo "-----------------------------------------------------------------"
+            echo "$output"
+            echo "-----------------------------------------------------------------"
+            exit 1
+        fi
     fi
 
     cd "$original_dir" || exit
