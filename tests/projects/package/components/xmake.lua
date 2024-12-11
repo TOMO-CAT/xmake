d6a2c78a17ba9190ad53572688f8bef670fa2c83
TOMOCAT <TOMO-CAT@github.com>
2024-12-11 17:48:47 +0800

    [Offline]: fix alsa-lib test

--- tests/projects/package/components/xmake.lua
+++ tests/projects/package/components/xmake.lua
@@ -2,23 +2,57 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("sfml")
 
-target("graphics")
+target("graphics", function()
     set_kind("static")
     add_files("src/graphics.cpp")
     add_packages("sfml", {components = "graphics", public = true})
+end)
 
-target("network")
+target("network", function()
     set_kind("static")
     add_files("src/network.cpp")
     add_packages("sfml", {components = "network", public = true})
+end)
 
-target("test")
+target("test", function()
     set_kind("binary")
     add_files("src/main.cpp")
     add_deps("graphics", "network")
+end)
 
-package("sfml")
+package("xtrans", function()
+    set_kind("library", {headeronly = true})
+    set_homepage("https://www.x.org/")
+    set_description("X.Org: X Network Transport layer shared code")
 
+    set_urls("https://www.x.org/archive/individual/lib/xtrans-$(version).tar.gz")
+    add_versions("1.4.0", "48ed850ce772fef1b44ca23639b0a57e38884045ed2cbb18ab137ef33ec713f9")
+
+    if is_plat("linux") then
+        add_extsources("apt::xtrans-dev", "pacman::xtrans")
+    end
+
+    if is_plat("macosx", "linux", "bsd") then
+        add_deps("pkg-config", "util-macros", "xorgproto")
+    end
+
+    on_install("macosx", "linux", "bsd", function (package)
+        local configs = {"--sysconfdir=" .. package:installdir("etc"),
+                         "--localstatedir=" .. package:installdir("var"),
+                         "--disable-dependency-tracking",
+                         "--disable-silent-rules",
+                         "--enable-docs=no"}
+        -- fedora systems do not provide sys/stropts.h
+        io.replace("Xtranslcl.c", "# include <sys/stropts.h>", "# include <sys/ioctl.h>")
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_ctypes("Xtransaddr", {includes = "X11/Xtrans/Xtrans.h"}))
+    end)
+end)
+
+package("sfml", function()
     set_homepage("https://www.sfml-dev.org")
     set_description("Simple and Fast Multimedia Library")
 
@@ -206,4 +240,78 @@ package("sfml")
             ]]}, {includes = "SFML/Network.hpp"}))
         end
     end)
-package_end()
+end)
+
+package("libsndio", function()
+    set_homepage("https://sndio.org")
+    set_description("Sndio is a small audio and MIDI framework part of the OpenBSD project and ported to FreeBSD, Linux and NetBSD")
+
+    set_urls("https://sndio.org/sndio-$(version).tar.gz")
+
+    add_versions("1.9.0", "f30826fc9c07e369d3924d5fcedf6a0a53c0df4ae1f5ab50fe9cf280540f699a")
+
+    if is_plat("linux") then
+        add_deps("alsa-lib")
+    end
+
+
+    on_install("linux", "bsd", "macosx", function (package)
+        import("package.tools.autoconf")
+        local configs = {}
+        local buildenvs = autoconf.buildenvs(package, {packagedeps = "alsa-lib"})
+        if not package:config("shared") then
+            io.replace("libsndio/Makefile.in",
+                "${CC} ${LDFLAGS} ${SO_CFLAGS} ${SO_LDFLAGS} -o ${SO} ${OBJS} ${LDADD}",
+                "${AR} ${ARFLAGS} libsndio.a ${OBJS}", {plain = true})
+            io.replace("libsndio/Makefile.in", "cp -R ${SO} ${SO_LINK} ${DESTDIR}${LIB_DIR}",
+                "cp libsndio.a ${DESTDIR}${LIB_DIR}", {plain = true})
+        end
+        autoconf.configure(package, configs, {envs = buildenvs})
+        os.vrunv("make", {}, {envs = buildenvs})
+        os.vrunv("make", {"install"}, {envs = buildenvs})
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("sio_open", {includes = "sndio.h"}))
+    -- end)
+end)
+
+package("alsa-lib", function()
+    set_homepage("https://alsa-project.org/wiki/Main_Page")
+    set_description("The Advanced Linux Sound Architecture (ALSA) provides audio and MIDI functionality to the Linux operating system.")
+    set_license("LGPL-2.1")
+
+    add_urls("http://www.alsa-project.org/files/pub/lib/alsa-lib-$(version).tar.bz2", {alias = "home"})
+    add_urls("https://github.com/alsa-project/alsa-lib/archive/refs/tags/v$(version).tar.gz", {alias = "github"})
+    add_versions("home:1.2.10", "c86a45a846331b1b0aa6e6be100be2a7aef92efd405cf6bac7eef8174baa920e")
+    add_versions("github:1.2.10", "f55749847fd98274501f4691a2d847e89280c07d40a43cdac43d6443f69fc939")
+
+    add_configs("versioned", {description = "pass flag --without-versioned", default = true, type = "boolean"})
+
+    if is_plat("linux") then
+        -- https://stackoverflow.com/questions/9145264/undefined-reference-to-log
+        add_syslinks("pthread", "m")
+        add_extsources("pacman::alsa-lib", "apt::libasound2-dev")
+    end
+
+    if not is_plat("windows") then
+        add_deps("autoconf", "automake", "libtool", "m4")
+    end
+
+    on_install("linux", function (package)
+        local configs = {}
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
+        if package:is_debug() then
+            table.insert(configs, "--enable-debug")
+        end
+        if package:config("versioned")then
+            table.insert(configs, "--without-versioned")
+        end
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("snd_pcm_open", {includes = "alsa/asoundlib.h"}))
+    -- end)
+end)
