f63cea1af474cea5934d6375d4f79ac027cb2b01
TOMOCAT <TOMO-CAT@github.com>
2025-01-23 13:01:36 +0800

    [Offline]: ignore lua-cjson warnings

--- core/src/lua-cjson/xmake.lua
+++ core/src/lua-cjson/xmake.lua
@@ -1,19 +1,13 @@
-target("lua-cjson")
+target("lua-cjson", function()
     set_kind("static")
-    set_warnings("all")
+    set_warnings("none") -- ignore warnings
     if is_config("runtime", "luajit") then
         add_deps("luajit")
     else
         add_deps("lua")
     end
-    if is_plat("windows") then
-        set_languages("c89")
-    end
     add_files("lua-cjson/*.c|fpconv.c")
     -- Use internal strtod() / g_fmt() code for performance and disable multi-thread
     add_defines("NDEBUG", "USE_INTERNAL_FPCONV")
     add_defines("XM_CONFIG_API_HAVE_LUA_CJSON", {public = true})
-    if is_plat("windows") then
-        add_defines("inline=__inline")
-    end
-
+end)
