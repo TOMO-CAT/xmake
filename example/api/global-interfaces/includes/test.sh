fd1a30b428cc32b4b729ac37d91472b9f0c44cd5
TOMOCAT <TOMO-CAT@github.com>
2025-03-22 12:05:01 +0800

    [Examples]: g++-aarch64-linux-gnu cross build

--- example/api/global-interfaces/includes/test.sh
+++ example/api/global-interfaces/includes/test.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cd "${SCRIPT_DIR}"
-
 xmake config --verbose --clean
 xmake build --verbose
-
-cd -
--- example/cross-build/g++-aarch64-linux-gnu/test.sh
+++ example/cross-build/g++-aarch64-linux-gnu/test.sh
@@ -19,9 +19,7 @@ function ok() {
 }
 
 # 安装交叉编译工具链
-sudo apt install g++-9-aarch64-linux-gnu -y || exit 1
-sudo mkdir -p /usr/aarch64-linux-gnu/usr/aarch64-linux-gnu
-sudo ln -svnf /usr/aarch64-linux-gnu/lib /usr/aarch64-linux-gnu/usr/aarch64-linux-gnu/lib
+sudo apt install g++-aarch64-linux-gnu -y || exit 1
 
 # 交叉编译必须禁用掉对应的环境变量(这相当于环境变量是最高优先级?), 否则会用 host clang++ 以及错误的系统头文件
 unset CPLUS_INCLUDE_PATH
@@ -32,10 +30,10 @@ unset CXX
 unset LD
 
 # 编译交叉编译版本 (必须用绝对路径, 否则 cmake 会报错找不到 aarch64-none-linux-gnu-g++ 等二进制)
-xmake f -yD -p cross --cross=aarch64-linux-gnu- --arch=armv8-a --toolchain=orin --sdk=/usr
-xmake b -yvrD
+xmake config --yes -p cross --cross=aarch64-linux-gnu- --arch=armv8-a --sdk=/usr
+xmake build --verbose --all
 
-output=`file build/cross/armv8-a/release/g++-9-aarch64-linux-gnu-cross` || exit -1
+output=`file build/cross/armv8-a/release/g++-aarch64-linux-gnu` || exit -1
 if [[ "$output" =~ "ARM" && "$output" =~ "aarch64" ]]; then
     ok "output file is ARM aarch64"
 else
@@ -44,9 +42,9 @@ else
 fi
 
 # 编译 host 版本
-xmake f -cyD
-xmake b -yvrD
-output=`file build/linux/x86_64/release/g++-9-aarch64-linux-gnu-cross` || exit -1
+xmake config --yes --verbose
+xmake build --verbose --all
+output=`file build/linux/x86_64/release/g++-aarch64-linux-gnu` || exit -1
 if [[ "$output" =~ "x86-64" ]]; then
     ok "output file is x86_64"
 else
