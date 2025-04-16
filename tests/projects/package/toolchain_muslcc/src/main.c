ccf3b7ecb58ad8459049d65573e89bd78ef3dcb0
TOMOCAT <TOMO-CAT@github.com>
2025-04-16 14:20:53 +0800

    [Hotfix]: failed test

--- tests/projects/package/toolchain_muslcc/src/main.c
+++ tests/projects/package/toolchain_muslcc/src/main.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <zlib.h>
-#ifdef HAVE_LIBPLIST
-#   include <plist/plist.h>
-#endif
 
 int main(int argc, char** argv)
 {
     printf("hello world!\n");
     inflate(0, 0);
-#ifdef HAVE_LIBPLIST
-    plist_new_dict();
-#endif
     return 0;
 }
