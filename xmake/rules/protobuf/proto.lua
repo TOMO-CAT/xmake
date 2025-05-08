e05ef5b7542831ee0df50dcba2f1bd0648e4ec69
TOMOCAT <TOMO-CAT@github.com>
2025-05-08 19:21:04 +0800

    [Offline]: protobuf fixme

--- xmake/rules/protobuf/proto.lua
+++ xmake/rules/protobuf/proto.lua
@@ -477,6 +477,8 @@ function build_cxfiles(target, batchjobs, sourcebatch, opt, sourcekind)
         }
         table.insert(nodenames, nodename)
 
+        -- FIXME: 如果 a.proto 中引用了 b.proto, a.pb.cc 会等待 a.proto 编译完成, 但是不会等待 b.proto 编译完成
+        --        这意味着编译 a.pb.cc 的时候, b.pb.h 可能还没有生成, 导致编译失败
         local cxfile_nodename = nodename .. "/" .. sourcekind
         nodes[cxfile_nodename] = {
             name = cxfile_nodename,
