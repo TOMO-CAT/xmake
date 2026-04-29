1f30b9a9ff7e79b17d794b279030ca73e35d546b
TOMO-CAT <TOMO-CAT@github.com>
2026-04-29 16:14:48 +0800

    [Offline]: show debug hint

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,3 +4,8 @@
 # 用法: source scripts/env.sh
 
 export XMAKE_PROGRAM_DIR="${PWD}/xmake"
+
+echo ""
+echo -e "  \033[1;36mTip:\033[0m 使用以下命令可以进行 Lua 代码调试:"
+echo -e "  \033[1;33mxrepo env -b emmylua_debugger -v -- xmake b -vrD\033[0m"
+echo ""
