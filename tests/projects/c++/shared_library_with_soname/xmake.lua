50b09517486c6f15eb3f2fefedd0723b461ed213
TOMO-CAT <TOMO-CAT@github.com>
2026-05-09 17:15:52 +0800

    [Offline]: remove xmake package from test

--- tests/projects/c++/shared_library_with_soname/xmake.lua
+++ tests/projects/c++/shared_library_with_soname/xmake.lua
@@ -2,14 +2,16 @@ add_rules("mode.debug", "mode.release")
 
 set_version("1.0.1", {soname = true})
 
-target("foo")
+target("foo", function()
     set_kind("shared")
     add_files("src/foo.cpp")
     after_build(function (target)
         print(target:soname())
     end)
+end)
 
-target("tests")
+target("tests", function()
     set_kind("binary")
     add_deps("foo")
     add_files("src/main.cpp")
+end)
